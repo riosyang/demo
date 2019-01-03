af4982b0bddc8494c492cb9bfb0960d712213705
Your Name <you@example.com>
2019-01-03 09:40:58 +0800

    hahah

--- src/main/java/com/wiggin/springboot/demo/serviceImpl/StudentServiceImpl.java
+++ src/main/java/com/wiggin/springboot/demo/serviceImpl/StudentServiceImpl.java
@@ -25,11 +25,7 @@ public class StudentServiceImpl implements StudentService {
     public Student queryById(int id) {
         Student student = new Student();
         student.setSex("女");
-<<<<<<< HEAD
-        student.setDepartment("这是一1");
-=======
-        student.setDepartment("测试haha");
->>>>>>> refs/remotes/origin/master
+        student.setDepartment("运维");
         student.setName("xdclass-wiggin");
         student.setBirth(new Date());
         return student;
